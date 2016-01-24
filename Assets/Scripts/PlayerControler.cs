6802acbbdce7b07c631606e40357ca4a43aa7e2e
CrimsonBlade <rutz.joe@gmail.com>
2016-01-24 14:58:43 -0500

    Video 7
    making shots move and appear when fired

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -7,12 +7,27 @@ public class Boundary
 	public float xMin, xMax, zMin, zMax;
 }
 
-public class PlayerControler : MonoBehaviour {
+public class PlayerControler : MonoBehaviour 
+{
 
 	public float speed;
 	public float tilt;
 	public Boundary boundary;
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
 
+	private float nextFire;
+
+	void Update()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) 
+		{
+		//	GameObject clone = 
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);//as GameObject;
+		}
+	}
 
 	void FixedUpdate()
 	{
